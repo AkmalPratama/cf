871c8476b500f71b59672094d9d6c333c128b16a
Akmal <m.akmalpratama@yahoo.co.id>
2022-12-04 20:19:50 +0700

    836: A, B AC

---
+++ 836/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	if (n & 1) {
+		for (int i = 0; i < n; i++) {
+			cout << "1 ";
+		}
+		cout << endl;
+	} else {
+		cout << "1 3";
+		for (int i = 2; i < n; i++) {
+			cout << " 2";
+		}
+		cout << endl;
+	}
+}
+
+int main () {
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
+
