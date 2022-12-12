4e3e48eb56278f96567dc21b87e0b757134eceab
Akmal <m.akmalpratama@yahoo.co.id>
2022-12-12 23:45:33 +0700

    G24: A AC

---
+++ G24/A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main () {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			int x;
+			cin >> x;
+		}
+		cout << "1 " << n << endl;
+	}
+	return 0;
+}
+
