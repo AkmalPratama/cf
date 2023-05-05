f417b342d24ea1852f663600003eb80a0938ea93
Akmal <m.akmalpratama@yahoo.co.id>
2023-05-05 23:37:28 +0700

    709: A AC

---
+++ 709/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	cout << n * m << endl;
+}
+
+int main () {
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
+
